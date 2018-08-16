d8b2174fca5a39e80a687b29e81b741af117e256
justinmeilinger <justinmeilinger62@gmail.com>
2018-08-15 21:56:37 -0400

    First Commit

---
+++ src/main/java/com/bracket/springbootweb/BracketGamingApplication.java
@@ -0,0 +1,12 @@
+package com.bracket.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BracketGamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BracketGamingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bracket/springbootweb/BracketGamingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bracket.springbootweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BracketGamingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
